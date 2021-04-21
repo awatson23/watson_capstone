feaa131e67a52491cb286a50f35bdf08cbe81398
unknown <acadenceonwatts@gmail.com>
2021-04-21 14:02:26 -0400

    push change

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,14 @@
 var socket = io.connect("http://localhost:3000");
 
-var gsrVal = 0;
+var sensorVal = 0;
+let slider;
 
 socket.on("prox", function(proximityValue){
 	//take value and write it to the object
-	gsrVal = proximityValue;
-    console.log(gsrVal)
+	sensorVal = proximityValue;
+    console.log(sensorVal)
 	//send coords value to draw function
-	draw(gsrVal)
+	draw(sensorVal)
 });
 
 
@@ -15,6 +16,9 @@ socket.on("prox", function(proximityValue){
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL)
     angleMode(DEGREES)
+
+    slider = createSlider(0,255 ,100);
+    slider.position(1300, 100);
 }
 
 function draw() {
@@ -23,20 +27,24 @@ function draw() {
     rotateX(60) 
 
     noFill()
-    stroke(255)
+
+    strokeWeight(sensorVal/25);
+
+    let blue = slider.value();
+    stroke(blue)
 
     /*fill(255, 204, 0);
     circle(500, 400, gsrVal);*/
     
-    console.log(gsrVal)
+    console.log(sensorVal)
 
     for (var i=0; i < 10; i++) {
         beginShape()
         for (var j = 0; j < 360; j += 10) {
-            var rad = i * 5
+            var rad = i * sensorVal/8
             var x = rad * cos(j)
             var y = rad * sin(j)
-            var z = sin(frameCount + i * 13) * gsrVal
+            var z = sin(frameCount + i * 13) * sensorVal*2
 
             vertex(x, y, z)
         }
